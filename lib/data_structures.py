7e38c8364cfca86913178793466e1311c3f27a4a
Navashub <navasmuller01@gmail.com>
2023-11-25 23:00:57 +0300

    completed task

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,36 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    max_heat_level = max(food["heat_level"] for food in spicy_foods)
+    return [food for food in spicy_foods if food["heat_level"] == max_heat_level]
 
 def print_spicy_foods(spicy_foods):
-    pass
-
+    for food in spicy_foods:
+        print(f"Name: {food['name']}, Cuisine: {food['cuisine']}, Heat Level: {food['heat_level']}")
+        
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return [food for food in spicy_foods if food['cuisine'].lower() == cuisine.lower()]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+
+    if not spiciest_foods:
+        print("No spiciest foods.")
+        return
+
+    print("Spiciest Foods:")
+    for food in spiciest_foods:
+        print(f"Name: {food['name']}, Cuisine: {food['cuisine']}, Heat Level: {food['heat_level']}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    if not spicy_foods:
+        return 0
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    return total_heat_level / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
